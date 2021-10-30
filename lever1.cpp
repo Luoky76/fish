50cbf128d767a286be504cf2471987d7a127f29e
xyb3 <90136323+xyb3@users.noreply.github.com>
2021-10-30 19:53:08 +0800

    重新上传

--- lever1.cpp
+++ lever1.cpp
@@ -1,16 +1,28 @@
 Level1:: Level1(){
-	int cnt;
-	cnt = rand()%4 + 2;
+	int cnt,flag=1;
+	vector<char>c;
+	cnt = rand()%9 + 2;
+	int kuo;
+	kuo = rand()%7;
+	if(kuo+3 > cnt)kuo = -1;
 	for(int i=1 ; i<= cnt ;i++){
 		int num;
 		num = rand() % 101;
 		a.push_back(num);
 	}
 	for(int i=1;i<cnt;i++){
-		int ope;
-		ope = rand() % 2;
-		if(ope) b.push_back('+');
-		else b.push_back('-');
+		int op;
+		op = rand() % 2;
+		if(op)ope.push_back('+');
+		else ope.push_back('-');
+	}
+	if(kuo == 0 ||b[kuo-1] == '-')flag=1;
+	for(int i=kuo;i<cnt-1;i++){
+		if(i<kuo||i>=kuo+3||!flag)c[i]=ope[i];
+		else if(flag){
+			if(ope[i]=='-')c[i]='+';
+			else c[i]='-';
+		}
 	}
 	for(int i=1;i<=cnt;i++){
 		if(i == 1){
@@ -18,7 +30,7 @@ Level1:: Level1(){
 			coutinue;
 		}
 		else{
-			if(b[i-1] == '+') ans += a[i];
+			if(c[i-1] == '+') ans += a[i];
 			else ans -= a[i];
 		}
 	}
@@ -38,7 +50,9 @@ string Level1::getpro(){
 			if(Num[j] == 0)continue;
 			output[cntstr++]='0'+Num[j];
 		}
-		if(b[j]!='/0')output[cntstr++]=b[i];
+		if(j==kuo+3)output[cntstr++]=')';
+		if(ope[j]!='/0')output[cntstr++]=ope[i];
+		if(j==kuo)output[cntstr++]='(';
 	}
 } 
 bool Level1::checkAns(string ans){
