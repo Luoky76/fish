8c38cbea8f8987acd7a2c16e77f1af98d42c634d
Luoky76 <1085752693@qq.com>
2021-10-30 22:31:58 +0800

    添加了getPro的实现

--- problem.cpp
+++ problem.cpp
@@ -14,5 +14,35 @@ Problem* Problem::getInstance(int level)
 
 string Problem::getPro()
 {
-	//DO SOMETHING
+	int cnta = a.size();   //数字个数
+	int cnto = ope.size(); //操作符个数
+	int x = 0;             //当前数字数组位置
+	int y = 0;             //当前符号数组位置
+	string Pro = "";
+	while (x < cnta || y < cnto)
+	{
+		while (ope[y] == '(' && y < cnto)
+		{
+			Pro = Pro + "(";
+			y++;
+		}
+		if (x < cnta)
+		{
+			stringstream temp;
+			temp << a[x++];
+			string tempp;
+			temp >> tempp;
+			Pro = Pro + tempp;
+		}
+		while (ope[y] == ')' && y < cnto)
+		{
+			Pro = Pro + ")";
+			y++;
+		}
+		if (y < cnto)
+		{
+			Pro = Pro + ope[y++];
+		}
+	}
+	return Pro;
 }
