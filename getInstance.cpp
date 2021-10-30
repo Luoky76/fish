df000e5805233f9dd2f5953754501365ed7d15c4
monkd <2439740168@qq.com>
2021-10-30 10:10:42 +0800

    getInstance的实现

---
+++ getInstance.cpp
@@ -0,0 +1,13 @@
+Problem* Problem::getInstance(int level)
+{
+	if(level == 1){
+		Problem Pro = new Level1;
+	}
+	else if(level == 2){
+		Problem Pro = new Level2;
+	}
+	else if(level == 3){
+		Problem Pro = new Level3;
+	}
+	return Pro;
+}	 
