53c084f34fa66246fe6b100d8401e797cfb595b9
Qiu_Zeyuan <Qiu_zeyuan@126.com>
2021-10-30 20:06:28 +0800

    debug

--- Level3.cpp
+++ Level3.cpp
@@ -4,12 +4,21 @@ Level3::Level3()
 {
     char opt[4] = {'+', '-', '*', '//'};
     srand(time(0));
-    setLim(0,10000);
+    setLim(0, 10000);
 
-    int x = rand()%9+2;
+    int x = rand() % 9 + 2;
     while (x--)
     {
-        a.push_back((rand()%(maxNum*100+1)) / (100.0));
+        bool flag = rand() % 2;
+        isInteger.push_back(flag);
+        if (flag)
+        {
+            a.push_back((rand() % (maxNum + 1)));
+        }
+        else
+        {
+            a.push_back((rand() % (maxNum * 100 + 1)) / (100.0));
+        }
         if (!x)
             ope.push_back(opt[rand() % 4]);
     }
