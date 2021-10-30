fd9f21d0b32f567993aabaf0b42cfd5175512357
fireflylxx <2965150024@qq.com>
2021-10-30 10:34:58 +0800

    server

--- server.cpp
+++ server.cpp
@@ -5,23 +5,19 @@ Server::Server()
 }
 void Server::setGrade(int newGrade)
 {
-	this.grade = newGrade;
+    this.grade = newGrade;
 	getProblemInstance();
 }
-bool Server::check(string ans)
+bool Server::check(string ans) //判断答案是否正确
 {
 	return problem->checkAns();		
-}//判断答案是否正确
+}
 string Server::getProblem()
 {
-	return problem->getPro();
-}	//调用问题的 getPro
+	return problem->getPro();  //调用问题的 getPro
+}	
 void Server::getProblemInstance()
 {
-	problem = Problem::getInstance(grade);
+    problem = Problem::getInstance(grade);
 }
-	private:
-	int grade;
-	Problem* problem;
-	void getProblemInstance();	//调用getInstance （传入grade） 获取problem实例 
-}
+	
