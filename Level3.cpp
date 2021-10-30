e36248a29503074c0180e798aa5f6f02ee809794
Qiu_Zeyuan <Qiu_zeyuan@126.com>
2021-10-30 19:50:19 +0800

    Level初始化

--- Level3.cpp
+++ Level3.cpp
@@ -2,10 +2,11 @@
 
 Level3::Level3()
 {
+    char opt[4] = {'+', '-', '*', '//'};
     srand(time(0));
-    int x = rand()%9+2;
     setLim(0,10000);
 
+    int x = rand()%9+2;
     while (x--)
     {
         a.push_back((rand()%(maxNum*100+1)) / (100.0));
